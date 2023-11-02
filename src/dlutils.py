75935452f86fb00d0d76a39884adfef1ff1406ef
Heitor <heitorventura21@gmail.com>
2023-11-02 19:16:25 -0300

    revert pos encode

--- src/dlutils.py
+++ src/dlutils.py
@@ -193,21 +193,21 @@ class ConvLSTM(nn.Module):
         return param
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
-        super(PositionalEncoding, self).__init__()
-        self.dropout = nn.Dropout(p=dropout)
+        def __init__(self, d_model, dropout=0.1, max_len=5000):
+            super(PositionalEncoding, self).__init__()
+            self.dropout = nn.Dropout(p=dropout)
 
-        pe = torch.zeros(max_len, d_model, dtype=torch.float)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model).float() * (-math.log(10000.0) / d_model))
-        pe += torch.sin(position * div_term)
-        pe += torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_buffer('pe', pe)
+            pe = torch.zeros(max_len, d_model)
+            position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+            div_term = torch.exp(torch.arange(0, d_model).float() * (-math.log(10000.0) / d_model))
+            pe += torch.sin(position * div_term)
+            pe += torch.cos(position * div_term)
+            pe = pe.unsqueeze(0).transpose(0, 1)
+            self.register_buffer('pe', pe)
 
-    def forward(self, x, pos=0):
-        x = x + self.pe[pos:pos+x.size(0)]
-        return self.dropout(x)
+        def forward(self, x, pos=0):
+            x = x + self.pe[pos:pos+x.size(0), :]
+            return self.dropout(x)
 
 class TransformerEncoderLayer(nn.Module):
     def __init__(self, d_model, nhead, dim_feedforward=16, dropout=0):
@@ -222,7 +222,7 @@ class TransformerEncoderLayer(nn.Module):
         self.activation = nn.LeakyReLU(True)
 
     def forward(self, src,src_mask=None, src_key_padding_mask=None, is_causal=None):
-        src2 = self.self_attn(src, src, src)[0]
+        src2 = self.self_attn(src, src, src, attn_mask=src_mask)[0]
         src = src + self.dropout1(src2)
         src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
         src = src + self.dropout2(src2)
@@ -243,9 +243,9 @@ class TransformerDecoderLayer(nn.Module):
         self.activation = nn.LeakyReLU(True)
 
     def forward(self, tgt, memory, tgt_mask=None, memory_mask=None, tgt_key_padding_mask=None, memory_key_padding_mask=None):
-        tgt2 = self.self_attn(tgt, tgt, tgt)[0]
+        tgt2 = self.self_attn(tgt, tgt, tgt, attn_mask=tgt_mask)[0]
         tgt = tgt + self.dropout1(tgt2)
-        tgt2 = self.multihead_attn(tgt, memory, memory)[0]
+        tgt2 = self.multihead_attn(tgt, memory, memory, attn_mask=memory_mask)[0]
         tgt = tgt + self.dropout2(tgt2)
         tgt2 = self.linear2(self.dropout(self.activation(self.linear1(tgt))))
         tgt = tgt + self.dropout3(tgt2)
